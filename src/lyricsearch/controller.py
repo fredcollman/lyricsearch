9137756ec629cd02ee02f5e8901737a714beb58d
Fred Collman <fredcollman@gmail.com>
2021-02-16 12:55:59 +0000

    Split helper functions from main code

--- src/lyricsearch/controller.py
+++ src/lyricsearch/controller.py
@@ -3,29 +3,7 @@ import json
 import requests
 
 from .logging import get_logger
-
-
-def count_words(lyrics):
-    return len(lyrics.split(" ")) if lyrics else 0
-
-
-class ArtistNotRecognised(ValueError):
-    pass
-
-
-def extract_artist_id(data):
-    try:
-        return data["artists"][0]["id"]
-    except (KeyError, IndexError):
-        raise ArtistNotRecognised()
-
-
-def extract_lyrics(data):
-    return data.get("lyrics") or ""
-
-
-def extract_titles(data):
-    return [work["title"] for work in data.get("works") or []]
+from .transforms import count_words, extract_artist_id, extract_lyrics, extract_titles
 
 
 class CachedFileRepository:
